26fe4a67873cc82b42a789810d92156cfab218fc
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2019-09-11 11:38:30 -0700

    choose section done with responsive

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,24 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+// mobile accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
 // slider-carousel
 $('.slider-carousel').slick({
   dots: false,
